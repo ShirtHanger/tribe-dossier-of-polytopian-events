85ba1a73619d2f24746ec5296df638d1a938ff86
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-21 09:12:01 -0500

    Refactored some of tribeData page into functions, added a lore toggle button

--- client/tribeData.js
+++ client/tribeData.js
@@ -10,6 +10,7 @@ const mediaDisplay = document.querySelector('#media-display')
 const tribeHeadDisplay = document.querySelector('#tribe-head-display')
 const tribeUnitDisplay = document.querySelectorAll('.tribe-unit-display')
 const tribeCard = document.querySelector('#tribe-card')
+const learnMoreButton = document.querySelector('#learn-more-button')
 
 window.addEventListener('load', async () => {
     console.log('PAGE IS LOADED')
@@ -18,59 +19,44 @@ window.addEventListener('load', async () => {
     let selectedTribe = sessionStorage.getItem('loadedTribe')
     console.log('On the previous page, you selected', selectedTribe)
 
+    learnMoreButton.textContent = `Learn more about the ${selectedTribe} tribe!`
+
     /* YIPEE! */
 
     let tribeDrill = await getTribeInfo('tribes', selectedTribe)
     let cultureDrill = await getTribeInfo('cultures', selectedTribe)
     let mediaDrill = await getTribeInfo('medias', selectedTribe)
 
-    console.log(tribeDrill[0].description) /* IDK why it's doing this, it seems to do this when I use Tribe Name Search */
-    /* Maybe it's the way I defined it in server.js... */
+    let tribeHead = tribeDrill.headImageURL
+    let tribeDescription = tribeDrill.description
+    let tribeUnit = tribeDrill.unitImageURL
+    let tribeMusic = new Audio(tribeDrill.theme)
+    let tribeAmbience = new Audio(tribeDrill.natureAmbience)
+    let tribeColor = tribeDrill.colorHex
 
-    tribeHeadDisplay.setAttribute('src', tribeDrill[0].headImageURL)
+    descriptionTextDisplay.textContent = tribeDescription
+    tribeHeadDisplay.setAttribute('src', tribeHead)
 
     for (unit of tribeUnitDisplay) {
-        unit.setAttribute('src', tribeDrill[0].unitImageURL)
+        unit.setAttribute('src', tribeUnit)
     }
 
-    tribeCard.style.backgroundColor = tribeDrill[0].colorHex
-
-
-    /* Creates iterable arrays of tribe lore and it's cooresponding picture
-    Indexes should link together in accordance to the lore and its year */
-
-    let loreBlurbs = cultureDrill[0].lore
-    let mediaLinks = mediaDrill[0].mediaURLs
-
-    let loreArray = []
-    let mediaArray = []
-
-    /* I should point out: This is a for IN loop, not a for OF, for IN works for objects */
-    /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in */
-
-    for (blurb in loreBlurbs) {
-        iterateOverObject(loreBlurbs, blurb, loreArray)
-    }
-
-    for (media in mediaLinks) {
-        iterateOverObject(mediaLinks, media, mediaArray)
-    }
-
-    randomLore = randNum(loreArray.length)
-
-    loreTextDisplay.textContent = loreArray[randomLore]
-    descriptionTextDisplay.textContent = tribeDrill[0].description
-
-    if (mediaArray[randomLore].includes('images')) { /* Links like these are considered https://static.wikia.nocookie.net/supertribes/images/7/79/Bardur_Tribe_Moon_2022.jpg */
-        mediaDisplay.innerHTML = `
-        <img src="${mediaArray[randomLore]}">
-        `
-    } else {
-        mediaDisplay.innerHTML = `
-        <iframe src="${mediaArray[randomLore]}" frameborder="0" allowfullscreen></iframe>`
-    }
+    tribeCard.style.backgroundColor = tribeColor
 
     
+    tribeAmbience.loop = true
+    tribeAmbience.play()
+
+    loadTribeLore(cultureDrill, mediaDrill)
+    
+})
+
+learnMoreButton.addEventListener('click', async () => {
+    let selectedTribe = sessionStorage.getItem('loadedTribe')
+    console.log('Toggling lore of:', selectedTribe)
+    let cultureDrill = await getTribeInfo('cultures', selectedTribe)
+    let mediaDrill = await getTribeInfo('medias', selectedTribe)
+    loadTribeLore(cultureDrill, mediaDrill)
 })
 
 
@@ -94,7 +80,52 @@ function iterateOverObject(object, objectKey, array) {
     }
 }
 
+/* Axios call function using name search */
 async function getTribeInfo(category, tribeName) {
     let response = await axios.get(`http://localhost:3001/${category}/name/${tribeName}`)
-    return response.data
+    return response.data[0] /* IDK why I have to do this, must be the way my name search works in backend */
+}
+
+/* Sets the lore properties of tribe data page */
+function toggleLore(randomLore, loreArray, mediaArray, loreTextDisplay) {
+    loreTextDisplay.textContent = loreArray[randomLore]
+
+    if (mediaArray[randomLore].includes('images')) { /* Links like these are considered https://static.wikia.nocookie.net/supertribes/images/7/79/Bardur_Tribe_Moon_2022.jpg */
+        mediaDisplay.innerHTML = `
+        <img src="${mediaArray[randomLore]}">
+        `
+    } else {
+        mediaDisplay.innerHTML = `
+        <iframe src="${mediaArray[randomLore]}" frameborder="0" allowfullscreen></iframe>`
+    }
+}
+
+/* Loads all content in second page, toggles when user requests different lore */
+
+function loadTribeLore(cultureDrill, mediaDrill) {
+
+    let loreBlurbs = cultureDrill.lore
+    let mediaLinks = mediaDrill.mediaURLs
+
+    /* Creates iterable arrays of tribe lore and it's cooresponding picture
+    Indexes should link together in accordance to the lore and its year */
+
+    let loreArray = []
+    let mediaArray = []
+
+    /* I should point out: This is a for IN loop, not a for OF, for IN works for objects */
+    /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in */
+
+    for (blurb in loreBlurbs) {
+        iterateOverObject(loreBlurbs, blurb, loreArray)
+    }
+
+    for (media in mediaLinks) {
+        iterateOverObject(mediaLinks, media, mediaArray)
+    }
+
+    randomLore = randNum(loreArray.length)
+
+    toggleLore(randomLore, loreArray, mediaArray, loreTextDisplay)
+    
 }
