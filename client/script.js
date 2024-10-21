1fe23f04d8f5bc42ad456d3e648a7963bf4f9dbf
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-20 21:29:27 -0500

    Cant get content to load on other HTML, gonna try to create a SECOND JS file, rollback just incase I mess up

--- client/script.js
+++ client/script.js
@@ -3,6 +3,12 @@
 /* Containers */
 const tribeListContainer = document.querySelector('.tribe-list-container')
 
+const loreTextDisplay = document.querySelector('#lore-text-display')
+const descriptionTextDisplay = document.querySelector('#description-text-display')
+const mediaDisplay = document.querySelector('#media-display')
+
+console.log(loreTextDisplay, descriptionTextDisplay, mediaDisplay) /* Returns null */
+
 /* Buttons */
 
 
@@ -12,16 +18,18 @@ window.addEventListener('load', async () => {
 
     for (tribe of tribeResponseInitial.data) {
         let tribeItem = document.createElement('article')
-        tribeItem.classList.add('tribe-pull-button')
+        tribeItem.classList.add('tribe-preview')
+        
         tribeItem.innerHTML = `
 
-        <div class="tribe-preview-pictures">
+        <div class="tribe-preview-picture">
         
         <img src="${tribe.headImageURL}">
         
         </div>
 
         <button id="tribe-link"><a href="tribeData.html">${tribe.name}<a></button>
+
         <p class="tribe-preview-description">${tribe.description}</p>`
 
 
@@ -29,7 +37,7 @@ window.addEventListener('load', async () => {
         tribeItem.style.backgroundColor = tribe.colorHex
     } 
 
-    tribePullButtons = document.querySelectorAll('.tribe-pull-button')
+    tribePreview = document.querySelectorAll('.tribe-preview')
     tribeLinks = document.querySelectorAll('#tribe-link')
 
 
@@ -40,29 +48,43 @@ window.addEventListener('load', async () => {
     for (let tribelink of tribeLinks) {
         tribelink.addEventListener('click', async () => {
 
-            console.log('YOU CLICKED ON:', tribelink.innerText)
-            let tribeResponseLoad = await axios.get(`http://localhost:3001/tribes/name/${tribelink.innerText}`)
-            let cultureResponse = await axios.get(`http://localhost:3001/cultures/name/${tribelink.innerText}`)
-            let mediaResponse = await axios.get(`http://localhost:3001/medias/name/${tribelink.innerText}`)
+            let selectedTribe = tribelink.innerText
 
-            /* Creates iterable arrays of tribe lore and it's cooresponding picture */
+            console.log('YOU CLICKED ON:', selectedTribe)
 
-            let loreBlurbs = cultureResponse.data.lore
-            let mediaLinks = mediaResponse.data.mediaURLs
+            let tribeDrill = await getTribeInfo('tribes', selectedTribe)
+            let cultureDrill = await getTribeInfo('cultures', selectedTribe)
+            let mediaDrill = await getTribeInfo('medias', selectedTribe)
+
+            /* Creates iterable arrays of tribe lore and it's cooresponding picture
+            Indexes should link together in accordance to the lore and its year */
+
+            let loreBlurbs = cultureDrill[0].lore
+            let mediaLinks = mediaDrill[0].mediaURLs
 
             let loreArray = []
             let mediaArray = []
 
-            for (blurb of loreBlurbs) {
-                loreArray.push(blurb)
+            /* I should point out: This is a for IN loop, not a for OF, for IN works for objects */
+            /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in */
+
+            for (blurb in loreBlurbs) {
+                iterateOverObject(loreBlurbs, blurb, loreArray)
             }
 
-            for (media of mediaLinks) {
-                mediaArray.push(media)
+            for (media in mediaLinks) {
+                iterateOverObject(mediaLinks, media, mediaArray)
             }
 
-            console.log(loreArray)
-            console.log(mediaArray)
+
+            randomLore = randNum(loreArray.length)
+
+            /* Supposed to load content in the other HTML page
+            Doesn't work because these are null ??? */
+
+            loreTextDisplay.textContent = loreArray[randomLore]
+            descriptionTextDisplay.textContent = tribeDrill.description
+
             
         })
     }
@@ -73,7 +95,9 @@ window.addEventListener('load', async () => {
 
 
 
-/* EVENT LISTENERS */
+/* EVENT LISTENERS - LANDING PAGE */
+
+/* EVENT LISTENERS - TRIBE INFORMATION PAGE */
 
 /* FUNCTIONS */
 
@@ -84,3 +108,18 @@ function randNum(maxNum) {
     randIndex = Math.floor(Math.random() * maxNum) // Copied this from my Pokemon Album Prework, edited for this
     return randIndex
   }
+
+/* Iterates over an object like an array, returns the value of each key */
+/* https://www.geeksforgeeks.org/how-to-iterate-over-a-javascript-object/ (#1) */
+
+function iterateOverObject(object, objectKey, array) {
+    if (object.hasOwnProperty(objectKey)) {
+        objectValue = object[objectKey]
+        array.push(objectValue)
+    }
+}
+
+async function getTribeInfo(category, tribeName) {
+    let response = await axios.get(`http://localhost:3001/${category}/name/${tribeName}`)
+    return response.data
+}
